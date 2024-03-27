bb88519229471c6c5b416f03ef804e2d710c908d
vigneshp25 <32658520+vigneshp25@users.noreply.github.com>
2024-03-27 10:52:56 +0530

    changes

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+var loginPage = document.getElementById("loginPage");
+var registrationPage = document.getElementById("registrationPage");
+var userLandingPage = document.getElementById("userLandingPage");
+var logoutPage = document.getElementById("logoutPage");
 var loginPageUsername = document.getElementById("login-username");
+var loginPagePassword = document.getElementById("login-password");
+var loginPageErrorMessage = document.getElementById("loginErrorMessage");
+var registrationPageUsername = document.getElementById("registration-username");
+var registrationPagePassword = document.getElementById("registration-password");
+var registrationPageEmail = document.getElementById("registration-email");
+var registrationPageUsernameErrorAlert = document.getElementById("registration-username-alert");
+var registrationPagePasswordErrorAlert = document.getElementById("registration-password-alert");
+var registrationPageEmailErrorAlert = document.getElementById("registration-email-alert");
+var goToRegistration = function () {
+    if (loginPage != null && registrationPage != null) {
+        loginPage.classList.add("displayNone");
+        registrationPage.classList.remove("displayNone");
+    }
+};
+var goToLogin = function () {
+    if (loginPage != null && registrationPage != null) {
+        registrationPage.classList.add("displayNone");
+        loginPage.classList.remove("displayNone");
+    }
+};
--- script.ts
+++ script.ts
@@ -1,1 +1,28 @@
+const loginPage = document.getElementById("loginPage");
+const registrationPage = document.getElementById("registrationPage");
+const userLandingPage = document.getElementById("userLandingPage");
+const logoutPage = document.getElementById("logoutPage");
+
 const loginPageUsername = document.getElementById("login-username");
+const loginPagePassword = document.getElementById("login-password");
+const loginPageErrorMessage = document.getElementById("loginErrorMessage")
+
+const registrationPageUsername = document.getElementById("registration-username");
+const registrationPagePassword = document.getElementById("registration-password");
+const registrationPageEmail = document.getElementById("registration-email");
+const registrationPageUsernameErrorAlert = document.getElementById("registration-username-alert");
+const registrationPagePasswordErrorAlert = document.getElementById("registration-password-alert");
+const registrationPageEmailErrorAlert = document.getElementById("registration-email-alert");
+
+let goToRegistration = ()=>{
+    if(loginPage != null && registrationPage != null){
+        loginPage.classList.add("displayNone");
+        registrationPage.classList.remove("displayNone");
+    }
+}
+let goToLogin = ()=>{
+    if(loginPage != null && registrationPage != null){
+        registrationPage.classList.add("displayNone");
+        loginPage.classList.remove("displayNone");
+    }
+}
